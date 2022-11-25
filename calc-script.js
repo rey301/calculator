e1058ab333a3464236b95a922e4ccf5ed878a68d
rey301 <abrahamrey3001@gmail.com>
2022-11-25 21:28:11 +0000

    Update output so decimal can only be clicked once

--- calc-script.js
+++ calc-script.js
@@ -33,35 +33,59 @@ function operate (operator, num1, num2) {
     }
 }
 
+const decBtn = document.querySelector('.decimal');
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', () => {
+    document.querySelector('.output').textContent = 0;
+    decBtn.addEventListener('click', e => {
+        document.querySelector('.output').textContent += e.target.textContent;
+        console.log(e); 
+    }, {once: true});
+
+    decBtn.addEventListener('click', function cb(e) {
+        e.currentTarget.removeEventListener(e.type, cb);
+    });
+});
+    
+decBtn.addEventListener('click', e => {
+    document.querySelector('.output').textContent += e.target.textContent;
+    console.log(e); 
+}, {once: true});
+
+// removes click event
+decBtn.addEventListener('click', function cb(e) {
+    e.currentTarget.removeEventListener(e.type, cb);
+});
+
 const numBtns = document.querySelectorAll('.number');
-
-
 numBtns.forEach(btn => {
     btn.addEventListener('click', e => {
         const output = document.querySelector('.output');
-        const decFlag = output.textContent.includes('.');
         const numText = e.target.textContent;
-        let outText = output.textContent.replace(/,/g, '');
+        let outText = output.textContent;
 
         if (outText === '0') {
-            outText = numText; 
-        } else if (outText.length < 9) {
+            return output.textContent = numText; 
+        } else if (outText.replace(/,|\./g, '').length < 9) {
             outText += numText;
         }
 
         // inserts comma for every third character
-        if (outText.length > 3 && outText.length <= 6) {
-            outText = outText.slice(0, outText.length-3) + 
-                ',' + 
-                outText.slice(outText.length-3);
-        } else if (outText.length > 6) {
-            outText = outText.slice(0, outText.length-6) + 
-                ',' + 
-                outText.slice(outText.length-6, outText.length-3) + 
-                ',' +
-                outText.slice(outText.length-3);
+        if (!outText.includes('.')) {
+            outText = outText.replace(/,/g, '');
+            if (outText.length > 3 && outText.length <= 6) {
+                outText = outText.slice(0, outText.length-3) + 
+                    ',' + 
+                    outText.slice(outText.length-3);
+            } else if (outText.length > 6) {
+                outText = outText.slice(0, outText.length-6) + 
+                    ',' + 
+                    outText.slice(outText.length-6, outText.length-3) + 
+                    ',' +
+                    outText.slice(outText.length-3);
+            }
         }
-
+        
         output.textContent = outText;
     })
 });
